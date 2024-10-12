bbbf0f9fbe51b08a37c80e0bb862a72d79cd51c0
hilal555 <muhamadhilal04@gmail.com>
2024-10-12 22:15:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.graphics.drawable.shapes.Shape
+
+fun ContohList() {
+    println("=== List ===")
+    // List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    //Menghapus data dari list Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+    // Mengubah data dalam list Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    // List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+
+}
+
+fun main(){
+    ContohList()
+}
