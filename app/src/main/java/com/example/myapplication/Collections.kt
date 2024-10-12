f11d21b6e807f4d05a97efcd0c07f14607acb8e6
hilal555 <muhamadhilal04@gmail.com>
2024-10-12 22:46:15 +0700

    Map read-only

--- app/src/main/java/com/example/myapplication/Collections.kt
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -50,7 +50,15 @@ fun ContohSet (){
     println(shapesLocked)
 }
 
-fun ContohMap
+fun ContohMap(){
+
+    println()
+    println("=== Map ===")
+
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+}
+
 fun main(){
     ContohList()
 
