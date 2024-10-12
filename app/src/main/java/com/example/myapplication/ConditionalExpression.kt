996e52da512122d0ec6f3bda6c534ff5db3b11b7
hilal555 <muhamadhilal04@gmail.com>
2024-10-12 23:00:41 +0700

    Membuat fungsi loop

--- app/src/main/java/com/example/myapplication/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication/ConditionalExpression.kt
@@ -9,4 +9,30 @@ fun ConditionalStatement() {
     } else {
         println("Negative Number")
     }
+
+    // When
+    val day = 1
+    when (day){
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> print("Invalid Day")
+    }
+
+    //try-catch
+    val value = "10"
+    try{
+        val result = value.toInt()
+        println("Result: $result")
+    }catch (e: NumberFormatException){
+        println("Invalid Number")
+    }
+}
+fun Perulangan(){
+    println()
+    println("=== Perulangan ===")
 }
